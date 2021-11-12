6e6f0680a650ef40763dc5b35725f75ecd5f02c8
Abner Chang <abner.chang@hpe.com>
2021-11-12 14:28:14 +0800

    RiscVPlatformPkg/U500: Add device tree for U500 platform
    Signed-off-by: Abner Chang <abner.chang@hpe.com>
    Cc: Daniel Schaefer <daniel.schaefer@hpe.com>
    Cc: Sunil V L <sunilvl@ventanamicro.com>
    Reviewed-by: Daniel Schaefer <daniel.schaefer@hpe.com>

---
+++ Platform/SiFive/U5SeriesPkg/FreedomU500VC707Board/DeviceTree/sifive-fu500-prci.h
@@ -0,0 +1,19 @@
+/**@file
+
+  SPDX-License-Identifier: (GPL-2.0 OR MIT)
+  Copyright (c) 2018-2019 SiFive, Inc
+
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+#ifndef __DT_BINDINGS_CLOCK_SIFIVE_FU500_PRCI_H
+#define __DT_BINDINGS_CLOCK_SIFIVE_FU500_PRCI_H
+
+/* Clock indexes for use by Device Tree data and the PRCI driver */
+
+#define PRCI_CLK_COREPLL	       0
+#define PRCI_CLK_DDRPLL		       1
+#define PRCI_CLK_GEMGXLPLL	       2
+#define PRCI_CLK_TLCLK		       3
+
+#endif

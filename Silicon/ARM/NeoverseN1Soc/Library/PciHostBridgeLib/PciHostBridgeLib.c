df870c7beaeedbbbc8cc66b42e26977cd00893a5
sahil <sahil@arm.com>
2022-12-08 17:37:07 +0530

    Silicon/ARM/NeoverseN1Soc: Update CCIX PNP ID
    The PNP ID 0x0A09 used for CCIX host bridge in the current code
    is not defined in any specification and is therefore incorrect.
    
    Also, there is no need for a separate ID for CCIX host bridge,
    for the following reasons:
    
    1. CCIX doesn't have any host specific requirements/ingredients
       as such.
    2. CCIX protocol messages flow over regular PCIe.
    3. CCIX devices and root ports are natively discovered using the
       CCIX DVSEC.
    
    Therefore, reuse PCIe PNP ID for CCIX.
    
    Signed-off-by: sahil <sahil@arm.com>
    Acked-by: Samer El-Haj-Mahmoud <Samer.El-Haj-Mahmoud@arm.com>
    Reviewed-by: Sami Mujawar <sami.mujawar@arm.com>

--- Silicon/ARM/NeoverseN1Soc/Library/PciHostBridgeLib/PciHostBridgeLib.c
+++ Silicon/ARM/NeoverseN1Soc/Library/PciHostBridgeLib/PciHostBridgeLib.c
@@ -65,8 +65,8 @@ STATIC EFI_PCI_ROOT_BRIDGE_DEVICE_PATH mEfiPciRootBridgeDevicePath[ROOT_COMPLEX_
           (UINT8)(sizeof (ACPI_HID_DEVICE_PATH) >> 8)
         }
       },
-      EISA_PNP_ID(0x0A09), // CCIX
-      0
+      EISA_PNP_ID(0x0A08), // CCIX
+      1
     },
     {
       END_DEVICE_PATH_TYPE,

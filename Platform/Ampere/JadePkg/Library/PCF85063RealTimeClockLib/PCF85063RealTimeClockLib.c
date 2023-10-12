7f25175ed42f8ec16e48e24cfcc8d7e25a676d78
Laszlo Ersek <lersek@redhat.com>
2023-10-12 11:09:44 +0200

    JadePkg/PCF85063RealTimeClockLib: hide LibRtcVirtualNotifyEvent
    The RealTimeClockLib class header in edk2 mistakenly declares a function
    called LibRtcVirtualNotifyEvent(). No component ever calls this function
    crossing module boundaries; all RealTimeClockLib instances in edk2 and
    edk2-platforms are supposed to register (and do register) their
    SetVirtualAddressMap() notification functions.
    
    Rename LibRtcVirtualNotifyEvent() to VirtualNotifyEvent(), and make it
    static, in preparation for removing the LibRtcVirtualNotifyEvent()
    declaration from the lib class header later.
    
    Build-tested only (with "Jade.dsc").
    
    Cc: Chuong Tran <chuong@os.amperecomputing.com>
    Cc: Leif Lindholm <quic_llindhol@quicinc.com>
    Cc: Nhi Pham <nhi@os.amperecomputing.com>
    Cc: Rebecca Cran <rebecca@os.amperecomputing.com>
    Ref: https://bugzilla.tianocore.org/show_bug.cgi?id=4564
    Signed-off-by: Laszlo Ersek <lersek@redhat.com>
    Reviewed-by: Ard Biesheuvel <ardb@kernel.org>

--- Platform/Ampere/JadePkg/Library/PCF85063RealTimeClockLib/PCF85063RealTimeClockLib.c
+++ Platform/Ampere/JadePkg/Library/PCF85063RealTimeClockLib/PCF85063RealTimeClockLib.c
@@ -201,9 +201,10 @@ LibSetWakeupTime (
   @param[in]    Event   The Event that is being processed
   @param[in]    Context Event Context
 **/
+STATIC
 VOID
 EFIAPI
-LibRtcVirtualNotifyEvent (
+VirtualNotifyEvent (
   IN EFI_EVENT        Event,
   IN VOID             *Context
   )
@@ -246,7 +247,7 @@ LibRtcInitialize (
   Status = gBS->CreateEventEx (
                   EVT_NOTIFY_SIGNAL,
                   TPL_NOTIFY,
-                  LibRtcVirtualNotifyEvent,
+                  VirtualNotifyEvent,
                   NULL,
                   &gEfiEventVirtualAddressChangeGuid,
                   &mVirtualAddressChangeEvent

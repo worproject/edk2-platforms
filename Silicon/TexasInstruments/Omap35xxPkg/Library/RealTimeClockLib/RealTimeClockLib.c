06f6274d56422084281886fad447ab117fd0e487
Laszlo Ersek <lersek@redhat.com>
2023-10-12 11:09:50 +0200

    Omap35xxPkg/RealTimeClockLib: drop LibRtcVirtualNotifyEvent
    The RealTimeClockLib class header in edk2 mistakenly declares a function
    called LibRtcVirtualNotifyEvent(). No component ever calls this function
    crossing module boundaries; all RealTimeClockLib instances in edk2 and
    edk2-platforms are supposed to register (and do register) their
    SetVirtualAddressMap() notification functions.
    
    In particular, Omap35xxPkg/RealTimeClockLib *itself* doesn't even use
    LibRtcVirtualNotifyEvent() -- the function is defined with an empty body,
    clearly in an understandable, but misguided, attempt, to conform to the
    (bogus) library interface. Remove the function.
    
    Build-tested only (with "BeagleBoardPkg.dsc").
    
    Cc: Leif Lindholm <quic_llindhol@quicinc.com>
    Ref: https://bugzilla.tianocore.org/show_bug.cgi?id=4564
    Signed-off-by: Laszlo Ersek <lersek@redhat.com>
    Reviewed-by: Ard Biesheuvel <ardb@kernel.org>

--- Silicon/TexasInstruments/Omap35xxPkg/Library/RealTimeClockLib/RealTimeClockLib.c
+++ Silicon/TexasInstruments/Omap35xxPkg/Library/RealTimeClockLib/RealTimeClockLib.c
@@ -271,21 +271,3 @@ LibRtcInitialize (
 
   return Status;
 }
-
-/**
-  Fixup internal data so that EFI can be call in virtual mode.
-  Call the passed in Child Notify event and convert any pointers in
-  lib to virtual mode.
-
-  @param[in]    Event   The Event that is being processed
-  @param[in]    Context Event Context
-**/
-VOID
-EFIAPI
-LibRtcVirtualNotifyEvent (
-  IN EFI_EVENT        Event,
-  IN VOID             *Context
-  )
-{
-  return;
-}

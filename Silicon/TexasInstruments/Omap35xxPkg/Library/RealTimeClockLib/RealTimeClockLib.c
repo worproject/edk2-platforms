b68199ec58956e1f98b953336c441188f2ea8dee
Laszlo Ersek <lersek@redhat.com>
2023-10-20 14:19:03 +0200

    Omap35xxPkg/RealTimeClockLib: remove superfluous instance init steps
    RealTimeClockLib instances are consumed by edk2's
    EmbeddedPkg/RealTimeClockRuntimeDxe driver. In its entry point function
    InitializeRealTimeClock(), the driver:
    
    (1) calls LibRtcInitialize(),
    
    (2) sets the GetTime(), SetTime(), GetWakeupTime() and SetWakeupTime()
        runtime services to its own similarly-named functions -- where those
        functions wrap the corresponding RealTimeClockLib APIs,
    
    (3) installs EFI_REAL_TIME_CLOCK_ARCH_PROTOCOL with a NULL protocol
        interface.
    
    Steps (2) and (3) conform to PI v1.8 sections II-9.7.2.4 through
    II-9.7.2.7.
    
    However, this means that LibRtcInitialize() (of any RealTimeClockLib
    instance) should not itself (a) set the GetTime(), SetTime(),
    GetWakeupTime() and SetWakeupTime() runtime services, nor (b) install
    EFI_REAL_TIME_CLOCK_ARCH_PROTOCOL. The runtime service pointers will be
    overwritten in step (2) anyway, and step (3) will uselessly install a
    second (NULL-interface) EFI_REAL_TIME_CLOCK_ARCH_PROTOCOL instance in the
    protocol database. (The protocol only serves to notify the DXE Foundation
    about said runtime services being available.)
    
    Clean up Omap35xxPkg/RealTimeClockLib accordingly.
    
    (Note that the lib instance INF file already does not list
    UefiRuntimeServicesTableLib and gEfiRealTimeClockArchProtocolGuid.)
    
    Build-tested only (with "BeagleBoardPkg.dsc").
    
    Cc: Ard Biesheuvel <ardb+tianocore@kernel.org>
    Cc: Leif Lindholm <quic_llindhol@quicinc.com>
    Ref: https://bugzilla.tianocore.org/show_bug.cgi?id=4565
    Signed-off-by: Laszlo Ersek <lersek@redhat.com>
    Message-Id: <20231020121903.44986-8-lersek@redhat.com>
    Acked-by: Ard Biesheuvel <ardb@kernel.org>

--- Silicon/TexasInstruments/Omap35xxPkg/Library/RealTimeClockLib/RealTimeClockLib.c
+++ Silicon/TexasInstruments/Omap35xxPkg/Library/RealTimeClockLib/RealTimeClockLib.c
@@ -10,11 +10,9 @@
 
 #include <Library/BaseMemoryLib.h>
 #include <Library/UefiBootServicesTableLib.h>
-#include <Library/UefiRuntimeServicesTableLib.h>
 #include <Library/DebugLib.h>
 #include <Library/IoLib.h>
 
-#include <Protocol/RealTimeClock.h>
 #include <Protocol/EmbeddedExternalDevice.h>
 
 #include <Omap3530/Omap3530.h>
@@ -242,7 +240,6 @@ LibRtcInitialize (
   )
 {
   EFI_STATUS    Status;
-  EFI_HANDLE    Handle;
   UINT8         Data;
   EFI_TPL       OldTpl;
 
@@ -255,19 +252,5 @@ LibRtcInitialize (
   ASSERT_EFI_ERROR(Status);
   gBS->RestoreTPL(OldTpl);
 
-  // Setup the setters and getters
-  gRT->GetTime       = LibGetTime;
-  gRT->SetTime       = LibSetTime;
-  gRT->GetWakeupTime = LibGetWakeupTime;
-  gRT->SetWakeupTime = LibSetWakeupTime;
-
-  // Install the protocol
-  Handle = NULL;
-  Status = gBS->InstallMultipleProtocolInterfaces (
-                  &Handle,
-                  &gEfiRealTimeClockArchProtocolGuid,  NULL,
-                  NULL
-                 );
-
-  return Status;
+  return EFI_SUCCESS;
 }

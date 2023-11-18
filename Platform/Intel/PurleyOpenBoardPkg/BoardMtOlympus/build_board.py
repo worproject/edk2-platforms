15e9fae959da804e52917c7d65566be2678ff0c0
Nate DeSimone <nathaniel.l.desimone@intel.com>
2023-11-17 22:20:11 -0800

    PurleyOpenBoardPkg: Replace Python Interpreter  Name
    Removes the assumption that the python interpreter will have the name
    python.
    
    Reviewed-by: Chasel Chiu <chasel.chiu@intel.com>
    Signed-off-by: Nate DeSimone <nathaniel.l.desimone@intel.com>

--- Platform/Intel/PurleyOpenBoardPkg/BoardMtOlympus/build_board.py
+++ Platform/Intel/PurleyOpenBoardPkg/BoardMtOlympus/build_board.py
@@ -1,7 +1,7 @@
 # @ build_board.py
 # This adds additional functions to the build_bios.py
 #
-# Copyright (c) 2019, Intel Corporation. All rights reserved.<BR>
+# Copyright (c) 2019 - 2023, Intel Corporation. All rights reserved.<BR>
 # SPDX-License-Identifier: BSD-2-Clause-Patent
 #
 
@@ -48,7 +48,7 @@ def pre_build_ex(config, functions):
     print("AML_FILTER= ", config.get("AML_FILTER"))
 
     # build the command with arguments
-    command = ["python",
+    command = [sys.executable,
                os.path.join(config["MIN_PACKAGE_TOOLS"],
                             "AmlGenOffset",
                             "AmlGenOffset.py"),
@@ -110,7 +110,7 @@ def post_build_ex(config, functions):
         print("post_build_ex Error")
         sys.exit(1)
 
-    common_patch_command = [os.path.join(config["PYTHON_HOME"], "python"),
+    common_patch_command = [sys.executable,
                             os.path.join(config["MIN_PACKAGE_TOOLS"],
                                          "PatchFv", "PatchBinFv.py"),
                             config["TARGET"],
@@ -131,7 +131,7 @@ def post_build_ex(config, functions):
             print("Patch Error!")
             sys.exit(1)
 
-    common_rebase_command = [os.path.join(config["PYTHON_HOME"], "python"),
+    common_rebase_command = [sys.executable,
                              os.path.join(config["MIN_PACKAGE_TOOLS"],
                                           "PatchFv", "RebaseBinFv.py"),
                              config["TARGET"],
